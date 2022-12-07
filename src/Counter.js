710fc8703889427f64b9619d5bc508918a7d3713
chinmaye <chinmayeravipati3234@gmail.com>
2022-12-07 18:09:31 +0530

    input added

--- src/Counter.js
+++ src/Counter.js
@@ -2,19 +2,28 @@ import {  useState } from "react";
 
 const Counter = () => {  
     const [count,setCount] = useState(0);
+    const [inputValue,setInputValue] = useState("")
     const increaseByOne = () => {
-        setCount(count + 1);
+        if(inputValue!=="")
+        setCount(count + parseInt(inputValue));
     };
     const decreaseByOne = () =>
     {
-        setCount(count - 1);
+        if(inputValue!=="")
+        setCount(count - parseInt(inputValue));
     };
     return(
         <div>
             <p>{count}</p>
+            <input type="number" value={inputValue} onChange={(e)=>{
+                setInputValue(e.target.value)
+            }}/>
             <button onClick={increaseByOne}>increase</button>
             <button onClick={decreaseByOne}>decrease</button>
-            <button onClick={()=>setCount(0)}>Reset</button>
+            <button onClick={()=>{
+                setCount(0);
+                setInputValue("")
+                }}>Reset</button>
         </div>
     );
 };
